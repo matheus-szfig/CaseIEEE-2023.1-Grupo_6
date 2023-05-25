06b89498d773345506b96eeefbf2d1b35a754b25
Arthur Duarte <duartearthur42@gmail.com>
2023-05-25 13:23:16 -0300

    Delete user feature working with token implemented

--- backend/src/core/controllers/users/delete.js
+++ backend/src/core/controllers/users/delete.js
@@ -1,13 +1,40 @@
-
 import { request, response } from "express";
 import { deleteUserService } from "../service/users/delete";
 
-
-
 export default async function DeleteUser(req = request, res = response) {
-    const {id} = req.params
-    const deleteService = await deleteUserService(id)
-    res.json(deleteService)
+	const deleteId = req.params.id;
+
+	if (!req.token?.UserInfo) {
+		res.status(401).send("Token is missing!");
+		return;
+	}
+
+	const { id = 0, permission = [] } = req.token.UserInfo;
+
+	if (!permission.includes("admin") && !id === deleteId) {
+		if (!req.token.UserInfo) {
+			res.status(403).send("User does not have permission!");
+			return;
+		}
+	}
+
+	const deleteService = await deleteUserService(deleteId);
+	res.json(deleteService);
 }
 
-
+// req.token = {
+//     UserInfo:{
+//         id,
+//         nome,
+//         email,
+//         permission:[
+//             '[descricao da permissao]'
+//         ],
+//         cargos:[
+//             {
+//                 equipe:'[nome da equipe]',
+//                 cargo:'[cargo]'
+//             }
+//         ]
+//     }
+// }
