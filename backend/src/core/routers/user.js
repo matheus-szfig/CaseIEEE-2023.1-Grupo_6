96083dced44f49746c6833cd29def374bc49b7a5
Lucas Teixeira <132716674+LucasTS13@users.noreply.github.com>
2023-05-29 19:31:10 -0300

    new feature

--- backend/src/core/routers/user.js
+++ backend/src/core/routers/user.js
@@ -1,10 +1,11 @@
 import { Router, request, response } from "express";
-import GetUsers from "../controllers/users/get";
+import { findAll, findOne } from "../controllers/users/get";
 import DeleteUser from "../controllers/users/delete";
 
 const router_usuario = Router();
 
-router_usuario.get("/", GetUsers);
+router_usuario.get("/", findAll);
+router_usuario.get("/:id", findOne);
 router_usuario.delete("/delete/:id", DeleteUser);
 // router_usuario.get("/", GetUsuarios);
 // router_usuario.get("/", GetUsuarios);
