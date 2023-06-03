0ab3de51dad9ba34f3500df07b9186dc476a1a20
Lucas Teixeira <132716674+LucasTS13@users.noreply.github.com>
2023-06-03 15:26:25 -0300

    Update get.js

--- backend/src/core/controllers/users/get.js
+++ backend/src/core/controllers/users/get.js
@@ -2,7 +2,8 @@ import { request, response } from "express";
 import { findAllService, findOneService } from "../../service/users/get";
 
 export async function findAll(req = request, res = response) {
-  res.json(findAllService);
+  const findAll = await findAllService();
+  res.json(findAll);
 }
 
 export async function findOne(req = request, res = response) {
