4cf67c9e49c4bb3b631e23fe6670d5a2310bdb61
Lucas Teixeira <132716674+LucasTS13@users.noreply.github.com>
2023-06-03 20:23:35 -0300

    Update get.js

--- backend/src/core/service/users/get.js
+++ backend/src/core/service/users/get.js
@@ -2,7 +2,10 @@ import { database } from "../../../config/database";
 
 export async function findAllService() {
     try {
-        const users = await database("usuario").select("id", "nome", "email")
+        const users = await database("usuario").select("usuario.id", "usuario.nome", "usuario.email", "equipe.nome as equipe", "cargo.nome as cargo")
+        .leftJoin("usuario_equipe", "usuario_equipe.id_usuario", "usuario.id")
+        .leftJoin("equipe", "equipe.id", "usuario_equipe.id_equipe")
+        .leftJoin("cargo", "cargo.id", "usuario_equipe.id_cargo")
         return users;
     } catch (error) {
         console.log(error);
@@ -14,7 +17,11 @@ export async function findAllService() {
 
 export async function findOneService(id) {
     try {
-        const user = await database("usuario").select("id", "nome", "email").where({id}).first()
+        const user = await database("usuario").select("usuario.id", "usuario.nome", "usuario.email", "equipe.nome as equipe", "cargo.nome as cargo")
+        .leftJoin("usuario_equipe", "usuario_equipe.id_usuario", "usuario.id")
+        .leftJoin("equipe", "equipe.id", "usuario_equipe.id_equipe")
+        .leftJoin("cargo", "cargo.id", "usuario_equipe.id_cargo")
+        .where("usuario.id", id).first()
         if(!user) {
             throw new Error("User not found");
         }
