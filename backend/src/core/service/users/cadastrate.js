cd4d44bdf8a94ccac249d8dd57fabfdccbd8406f
Gustavo  Barros <gustavoandrade0125@gmail.com>
2023-06-05 21:46:53 -0300

    Versão pré-toast

--- backend/src/core/service/users/cadastrate.js
+++ backend/src/core/service/users/cadastrate.js
@@ -5,34 +5,49 @@ function verificaOpcao(valor, opcoes) {
 	return opcoes.some(opcao => opcao.nome === valor);
 }
 
-export async function cadastrateUserService(email, senha,nome,senhaConfirmacao, cargos) {
+export async function cadastrateUserService(email, senha,nome,senhaConfirmacao, cargos,permissao) {
 	try {
+		if(nome==="" || nome===null || nome==undefined ||
+			senha==="" || senha===null || senha==undefined ||
+			senhaConfirmacao==="" || senhaConfirmacao===null || senhaConfirmacao==undefined)
+			{
+				throw new Error("Preencha os campos obrigatórios.");
+			}
+
 		const equipeOpcoes = await database('equipe').select('nome');
 		const cargoOpcoes = await database('cargo').select('nome');
-
 		if(cargos.length < 1 || cargos.length > 3){
 			throw new Error("Informe de cargos ou equipes irregular.");
 		}
 
 		cargos.forEach(c => {
-			const verificador = verificaOpcao(c['cargo'].toLowerCase(), cargoOpcoes) && verificaOpcao(c['equipe'].toLowerCase(), equipeOpcoes);
+			const verificador = verificaOpcao(c['cargo'], cargoOpcoes) && verificaOpcao(c['equipe'], equipeOpcoes);
+			console.log(c['cargo']);
+
 			if(!verificador){
 				throw new Error("Cargo inválido");
 			}
 		});
 
 		if(senha.length<4){
-            throw new Error("Too short password");
+            throw new Error("Senha muito curta!");
         }
 
         if(senha!==senhaConfirmacao){
-
-            throw new Error("Passwords need to be equal");
+            throw new Error("As senhas precisam ser iguais");
         }
 
 		const userEmail = await database("usuario").select("*").where({email}).first();
 		if (userEmail) {
-			throw new Error("Email adress alredy cadastrated!");
+			throw new Error("Endereço de e-mail já cadastrado!");
+		}
+
+		if (permissao) {
+			permissao = 0;
+		} else if (!permissao) {
+			permissao = 1;
+		} else {
+			throw new Error("Erro no informe de permissões");
 		}
 
         const hash = await HashPassword(senha)
@@ -40,31 +55,30 @@ export async function cadastrateUserService(email, senha,nome,senhaConfirmacao, 
             email,
             senha: hash,
             nome,
+			notify : permissao
         })
 
 		const idUser = await database('usuario').select('id').where({"email": email }).first();
 		for(let i=0;i<3;i++){
-			if (cargos[i] === "" || equipes[i] === "") {
+			if (cargos[i]==="" || cargos[i]===undefined || cargos[i]===null) {
 				break;
 			}
-			let cargoUser = await database('cargo').select('id').where({"nome" : cargos[i]}).first()
-			let equipesUser = await database('equipe').select('id').where({"nome" : equipes[i]}).first()
+			let cargoUser = await database('cargo').select('id').where({"nome" : cargos[i].cargo}).first()
+			let equipesUser = await database('equipe').select('id').where({"nome" : cargos[i].equipe}).first()
 			await database("usuario_equipe").insert({
 				id_usuario : idUser.id,
 				id_cargo : cargoUser.id,
 				id_equipe  : equipesUser.id,
 				aprovado : 0
-			})
-
+			})	
 		}
 
 		return {
 			status: true,
-			message: "User successfully cadastrated!",
+			message: "Usuário cadastrado com êxito",
 		};
 
 	} catch (error) {
-        console.log(error);
 		return {
 			status: false,
 			message:error['message'],
--- frontend/src/pages/Cadastro.jsx
+++ frontend/src/pages/Cadastro.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useState } from "react";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 const Cadastro = () => {
   const [nome, setNome] = useState("");
@@ -14,22 +17,63 @@ const Cadastro = () => {
   const [cargo3, setCargo3] = useState("");
   const [permissao, setPermissao] = useState("");
 
-  function handleSubmit(event){
+  //const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handlePasswordToggle = () => {
+    setShowPassword(!showPassword);
+    console.log(showPassword);
+  };
+
+  async function handleSubmit(event){
     event.preventDefault();
-    console.log(nome, email, senha, senhaConfirmacao, equipe1,cargo1,equipe2,cargo2,equipe3,cargo3,permissao);
+    console.log("Info: ",nome, email, senha, senhaConfirmacao, equipe1,equipe2,equipe3,cargo1,cargo2,cargo3,permissao);
+    const usuario = {nome,email,senha,senhaConfirmacao,equipe1,cargo1,equipe2,cargo2,equipe3,cargo3,permissao};
 
-    /*const usuario = {nome, email, senha, senhaConfirmacao, equipe1,cargo1,equipe2,cargo2,equipe3,cargo3,permissao};
-
-    try {
+    try{
       const response = await axios.post("http://localhost:5000/user/cadaster", usuario);
       console.log(response.data);
     } catch (error) {
       console.error(error);
-    }*/
+      alert(error);
+    }
+    //navigate('/login')
   }
 
   function handleChange(event, setText) {
+    setText(event.target.value); 
+  }
+
+  const [verificador, setVerificador] = useState(true);
+  const [verificador2, setVerificador2] = useState(true);
+  function handleChangeSelect(event, setText) {
     setText(event.target.value);
+
+    let equipe1 = document.getElementById('e1Select');
+    let equipe2 = document.getElementById('e2Select');
+    let equipe3 = document.getElementById('e3Select');
+    let cargo1 = document.getElementById('c1Select');
+    let cargo2 = document.getElementById('c2Select');
+    let cargo3 = document.getElementById('c3Select');
+    if(equipe1.value!=="" && cargo1.value!==""){
+      setVerificador(false)
+    } else{
+      setVerificador(true)
+      equipe2.value="";
+      equipe2.selectedIndex = 0;
+      cargo2.value="";
+      cargo2.selectedIndex = 0;
+    }
+    if(equipe2.selectedIndex !== 0 && cargo2.selectedIndex !== 0){
+      setVerificador2(false)
+    } else{
+      setVerificador2(true)
+      equipe3.value="";
+      equipe3.selectedIndex = 0;
+      cargo3.value="";
+      cargo3.selectedIndex = 0;
+    }
+
   }
 
   function handleCheckboxChange(event) {
@@ -47,14 +91,14 @@ const Cadastro = () => {
 
       <div class="flex flex-col justify-center items-center w-screen h-fit">
           <div class="w-[90%] h-fit bg-white p-4 border-2 border-[#0D5FAA] rounded-[4px]">
-            <h1 className="underline underline-offset-4 decoration-black text-2xl text-[#0D5FAA] font-bold">CADASTRO DE USUÁRIOS</h1>
+            <h1 className="underline underline-offset-4 decoration-black text-2xl text-[#0D5FAA] font-bold" style={{ textDecorationColor: 'rgb(148 163 184)' }}>CADASTRO DE USUÁRIOS</h1>
 
             <form onSubmit={handleSubmit}>
 
             <section className="w-full h-[84%]">
 
               <div className='mt-3'>
-                <label for="nomeInput" className='text-blue-500 font-medium text-lg'>Nome<span className='text-[#F81818]'>*</span> </label>
+                <label for="nomeInput" id="nome" className='text-blue-500 font-medium text-lg'>Nome<span className='text-[#F81818]'>*</span> </label>
                 <input id="nomeInput" type="text" onChange={(event) => handleChange(event, setNome)} name="NomeUsuario" placeholder="ex: Gustavo Barros Rosa de Andrade" className="w-full h-[23%] p-2 border-2 focus:outline-none focus:shadow-none rounded-[4px]"/>
               </div>
 
@@ -65,13 +109,23 @@ const Cadastro = () => {
 
               <div className='mt-3'>
                 <div className="inline-block w-1/2 h-full pr-2">
-                  <label for="senhaInput" className='text-blue-500 font-medium text-lg'>Senha<span className='text-[#F81818]'>*</span> </label>
-                  <input id="senhaInput" type="password" onChange={(event) => handleChange(event, setSenha)} name="SenhaUsuario" placeholder='**************' className="w-[100%] h-[23%] p-2 border-2 focus:outline-none focus:shadow-none rounded-[4px]"/>
+                  <label for="senhaInput" className='text-blue-500 font-medium text-lg inline'>Senha<span className='text-[#F81818]'>*</span></label>
+                  <div className="relative">
+                    <input onChange={(event) => handleChange(event, setSenha)} type={showPassword ? "text" : "password"} name="SenhaUsuario" id="senha" placeholder={showPassword ? "exemploDeSenha4567" : "******************"} className="w-[100%] h-[23%] p-2 border-2 focus:outline-none focus:shadow-none rounded-[4px]"/>
+                    <span className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer " onClick={handlePasswordToggle}>
+                      {showPassword ? ( <img className="h-6" src={process.env.PUBLIC_URL + "/Visibility.svg"} alt="Show Password"/>) : (<img className="h-6" src={process.env.PUBLIC_URL + "/VisibilityOff.svg"} alt="Hide Password"/> )}
+                    </span>
+                  </div>
                 </div>
                 
                 <div className="inline-block w-1/2 h-full pl-2">
                   <label for="senhaConfInput" className='text-blue-500 font-medium text-lg'>Confirmação de Senha<span className='text-[#F81818]'>*</span> </label>
-                  <input id="senhaConfInput" type="password" onChange={(event) => handleChange(event, setSenhaConfirmacao)} name="ComfimacaoSenhaUsuario" placeholder='**************' className="w-[100%] h-[23%] p-2 border-2 focus:outline-none focus:shadow-none rounded-[4px]"/>
+                  <div className="relative">
+                    <input onChange={(event) => handleChange(event, setSenhaConfirmacao)} type={showPassword ? "text" : "password"} name="SenhaUsuario" id="senhaConfirma" placeholder={showPassword ? "exemploDeSenha4567" : "******************"} className="w-[100%] h-[23%] p-2 border-2 focus:outline-none focus:shadow-none rounded-[4px]"/>
+                    <span className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer " onClick={handlePasswordToggle}>
+                      {showPassword ? ( <img className="h-6" src={process.env.PUBLIC_URL + "/Visibility.svg"} alt="Show Password"/>) : (<img className="h-6" src={process.env.PUBLIC_URL + "/VisibilityOff.svg"} alt="Hide Password"/> )}
+                    </span>
+                  </div>
                 </div>
               </div>
 
@@ -79,9 +133,11 @@ const Cadastro = () => {
                  <div className="inline-block w-1/2 h-full pr-2">
                   <label for="e1Select" className='text-blue-500 font-medium text-lg'>Primeira Equipe<span className='text-[#F81818]'>*</span> </label>
 
-                   <select id="e1Select" name="equipe1" onChange={(event) => handleChange(event, setEquipe1)} className='w-[100%] h-[23%] text-gray-600 p-2 border-2 focus:shadow-none hover:cursor-pointer rounded-[4px]'>
-                      <option className="text-gray-300" value="">Selecione uma equipe</option>
+                   <select id="e1Select" name="equipe1" onChange={(event) => handleChangeSelect(event, setEquipe1)} className='w-[100%] h-[23%] p-2 border-2 focus:shadow-none hover:cursor-pointer rounded-[4px]'>
+                      <option className="text-gray-300" value="">-Selecione uma equipe-</option>
                       <option className="text-black" value="WIE">WIE</option>
+                      <option className="text-black" value="Marketing">Markrting</option>
+                      <option className="text-black" value="Gestão">Gestão</option>
                       <option className="text-black" value="RocketWolf">RocketWolf</option>
                       <option className="text-black" value="WolfBotz">WolfBotz</option>
                       <option className="text-black" value="SocialWolf">SocialWolf</option>
@@ -93,8 +149,8 @@ const Cadastro = () => {
                  <div className="inline-block w-1/2 h-full pl-2">
                    <label for="c1Select" className='text-blue-500 font-medium text-lg'>Primeiro Cargo<span className='text-[#F81818]'>*</span> </label>
 
-                   <select id="c1Select" name="cargo1" onChange={(event) => handleChange(event, setCargo1)} className='w-[100%] h-[23%] text-gray-600 p-2 border-2 focus:shadow-none hover:cursor-pointer rounded-[4px]'>
-                    <option className="text-gray-300" value="">Selecione um cargo</option>
+                   <select id="c1Select" name="cargo1" onChange={(event) => handleChangeSelect(event, setCargo1)} className='w-[100%] h-[23%] p-2 border-2 focus:shadow-none hover:cursor-pointer rounded-[4px]'>
+                    <option className="text-gray-300" value="">-Selecione um cargo-</option>
                     <option className="text-black" value="Presidente">Presidente</option>
                     <option className="text-black" value="Vice-Presidente">Vice-Presidente</option>
                     <option className="text-black" value="Gestão de Pessoas">Gestão de Pessoas</option>
@@ -113,9 +169,11 @@ const Cadastro = () => {
                  <div className="inline-block w-1/2 h-full pr-2">
                   <label for="e2Select" className='text-blue-500 font-medium text-lg'>Segunda Equipe</label>
 
-                   <select id="e2Select" name="equipe2" onChange={(event) => handleChange(event, setEquipe2)} className='w-[100%] h-[23%] text-gray-600 p-2 border-2 focus:shadow-none hover:cursor-pointer rounded-[4px]'>
-                      <option className="text-gray-300" value="">Selecione uma equipe</option>
+                   <select disabled={verificador} id="e2Select" name="equipe2" onChange={(event) => handleChangeSelect(event, setEquipe2)} className='w-[100%] h-[23%] p-2 border-2 focus:shadow-none hover:cursor-pointer rounded-[4px]'>
+                      <option className="text-gray-300" value="">-Selecione uma equipe-</option>
                       <option className="text-black" value="WIE">WIE</option>
+                      <option className="text-black" value="Marketing">Markrting</option>
+                      <option className="text-black" value="Gestão">Gestão</option>
                       <option className="text-black" value="RocketWolf">RocketWolf</option>
                       <option className="text-black" value="WolfBotz">WolfBotz</option>
                       <option className="text-black" value="SocialWolf">SocialWolf</option>
@@ -127,8 +185,8 @@ const Cadastro = () => {
                  <div className="inline-block w-1/2 h-full pl-2">
                    <label for="c2Select" className='text-blue-500 font-medium text-lg'>Segundo Cargo</label>
 
-                   <select id="c2Select" name="cargo2" onChange={(event) => handleChange(event, setCargo2)} className='w-[100%] h-[23%] text-gray-600 p-2 border-2 focus:shadow-none hover:cursor-pointer rounded-[4px]'>
-                    <option className="text-gray-300" value="">Selecione um cargo</option>
+                   <select disabled={verificador} id="c2Select" name="cargo2" onChange={(event) => handleChangeSelect(event, setCargo2)} className='w-[100%] h-[23%] p-2 border-2 focus:shadow-none hover:cursor-pointer rounded-[4px]'>
+                    <option className="text-gray-300" value="">-Selecione um cargo-</option>
                     <option className="text-black" value="Presidente">Presidente</option>
                     <option className="text-black" value="Vice-Presidente">Vice-Presidente</option>
                     <option className="text-black" value="Gestão de Pessoas">Gestão de Pessoas</option>
@@ -147,9 +205,11 @@ const Cadastro = () => {
                  <div className="inline-block w-1/2 h-full pr-2">
                   <label for="e3Select" className='text-blue-500 font-medium text-lg'>Terceira Equipe</label>
 
-                   <select id="e3Select" name="equipe3" onChange={(event) => handleChange(event, setEquipe3)} className='w-[100%] h-[23%] text-gray-600 p-2 border-2 focus:shadow-none hover:cursor-pointer rounded-[4px]'>
-                      <option className="text-gray-300" value="">Selecione uma equipe</option>
+                   <select disabled={verificador2} id="e3Select" name="equipe3" onChange={(event) => handleChangeSelect(event, setEquipe3)} className='w-[100%] h-[23%] p-2 border-2 focus:shadow-none hover:cursor-pointer rounded-[4px]'>
+                      <option className="text-gray-300" value="">-Selecione uma equipe-</option>
                       <option className="text-black" value="WIE">WIE</option>
+                      <option className="text-black" value="Marketing">Markrting</option>
+                      <option className="text-black" value="Gestão">Gestão</option>
                       <option className="text-black" value="RocketWolf">RocketWolf</option>
                       <option className="text-black" value="WolfBotz">WolfBotz</option>
                       <option className="text-black" value="SocialWolf">SocialWolf</option>
@@ -161,8 +221,8 @@ const Cadastro = () => {
                  <div className="inline-block w-1/2 h-full pl-2">
                    <label for="c3Select" className='text-blue-500 font-medium text-lg'>Terceiro Cargo</label>
 
-                   <select id="c3Select" name="cargo3" onChange={(event) => handleChange(event, setCargo3)} className='w-[100%] h-[23%] text-gray-600 p-2 border-2 focus:shadow-none hover:cursor-pointer rounded-[4px]'>
-                    <option className="text-gray-300" value="">Selecione um cargo</option>
+                   <select disabled={verificador2} id="c3Select" name="cargo3" onChange={(event) => handleChangeSelect(event, setCargo3)} className='w-[100%] h-[23%] p-2 border-2 focus:shadow-none hover:cursor-pointer rounded-[4px]'>
+                    <option className="text-gray-300" value="">-Selecione um cargo-</option>
                     <option className="text-black" value="Presidente">Presidente</option>
                     <option className="text-black" value="Vice-Presidente">Vice-Presidente</option>
                     <option className="text-black" value="Gestão de Pessoas">Gestão de Pessoas</option>
@@ -181,14 +241,14 @@ const Cadastro = () => {
             <section className="w-full h-[8%]">
             <div className='mt-4'>
               <div className="inline-block items-center w-1/2 h-full">
-                <input onChange={(event) => handleCheckboxChange(event, setPermissao)} className="w-3 h-3 mr-2 hover:cursor-pointer" type="checkbox" id="permission"/>
+                <input onChange={(event) => handleCheckboxChange(event, setPermissao)} className="rounded-full w-5 h-5 mr-2 mb-1 hover:cursor-pointer" type="checkbox" id="permission"/>
                 <label className="text-blue-500 font-medium text-ms hover:cursor-pointer" for="permission">Aceito receber notificações via email</label>
               </div>
                  
               <div className="inline-block items-center w-1/2 h-full">
                 <div className='w-full h-full flex items-center justify-end'>
-                  <input type="button" value="Cancelar" className="w-[35%] min-w-[65px] h-8 bg-[#F81818] text-white font-medium text-ms hover:cursor-pointer hover:underline hover:underline-offset-2 rounded-[4px]"/>
-                  <input type="submit" value="Enviar"  className="ml-10 w-[35%] min-w-[65px] h-8 bg-[#0D5FAA] text-white font-medium text-ms hover:cursor-pointer hover:underline hover:underline-offset-2 rounded-[4px]"/>
+                  <input /*onClick={() => navigate('/login')}*/ type="button" value="Cancelar" className="w-[35%] min-w-[65px] h-8 bg-[#F81818] text-white font-medium text-ms hover:cursor-pointer focus:underline hover:underline hover:underline-offset-2 rounded-[4px]"/>
+                  <input type="submit" value="Enviar"  className="ml-10 w-[35%] min-w-[65px] h-8 bg-[#0D5FAA] text-white font-medium text-ms hover:cursor-pointer focus:underline hover:underline hover:underline-offset-2 rounded-[4px]"/>
                 </div>
               </div>
             </div>
