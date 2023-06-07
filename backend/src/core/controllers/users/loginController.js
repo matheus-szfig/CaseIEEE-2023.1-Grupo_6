5c2612adaf4d7fe1f98446b92e32d7d7599734b7
laraFRamos <lara.fernandes.ramos2003@gmail.com>
2023-06-06 22:11:24 -0300

    Correção de erros no login.js e controller

--- backend/src/core/controllers/users/loginController.js
+++ backend/src/core/controllers/users/loginController.js
@@ -2,9 +2,9 @@ import { request, response } from "express";
 import { loginUserService } from "../../service/users/login";
 
 export default async function LoginUser(req = request, res = response) {
-  const { email, password } = req.body;
+  const { email, senha } = req.body;
 
-  const loginService = await loginUserService(email, password, res);
+  const loginService = await loginUserService(email, senha, res);
   res.json(loginService);
 
 }
