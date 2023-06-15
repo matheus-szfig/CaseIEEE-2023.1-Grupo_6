54f08033f22c15aa711ab52e6dc48e54e0bc9d82
Arthur Duarte <duartearthur42@gmail.com>
2023-06-15 15:35:27 -0300

    Inactivate funcionando, delete ainda será decidido

--- backend/src/core/service/users/delete.js
+++ backend/src/core/service/users/delete.js
@@ -1,24 +1,36 @@
-
 import { database } from "../../../config/database";
 
 export async function deleteUserService(id) {
-	try {
-		const user = await database("usuario").select("*").where({id, ativo:1});
-        
-		if (user.length === 0) {
-			throw new Error("User not find!");
-		}
+  const transaction = await database.transaction(); // Inicia a transação
 
-		const userDeleted = await database("usuario").update({ativo: 0}).where({ id });
-		return {
-			status: true,
-			message: "User deleted!",
-		};
-	} catch (error) {
-        console.log(error);
-		return {
-			status: false,
-			message:error['message'],
-		};
-	}
+  try {
+    const user = await transaction("usuario").select("*").where({ id, ativo: 1 });
+
+    if (user.length === 0) {
+      throw new Error("User not found!");
+    }
+
+    await transaction("usuario_equipe").update({ id_usuario: 1 }).where({ id_usuario: id });
+    await transaction("usuario_permissao").update({ id_usuario: 1 }).where({ id_usuario: id });
+    await transaction("voto_equipe").update({ id_usuario_ator: 1 }).where({ id_usuario_ator: id });
+    await transaction("voto_usuario").update({ id_usuario_ator: 1 }).where({ id_usuario_ator: id });
+    await transaction("usuario").where({ id }).delete();
+
+    await transaction.commit(); // Confirma a transação
+
+    return {
+      status: true,
+      message: "User deleted!",
+    };
+  } catch (error) {
+    console.log(error);
+    await transaction.rollback(); // Desfaz a transação em caso de erro
+
+    return {
+      status: false,
+      message: error.message,
+    };
+  } finally {
+    transaction.release(); // Libera a transação
+  }
 }
