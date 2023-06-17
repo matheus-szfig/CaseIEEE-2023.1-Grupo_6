ee39c15839214ef4a5a192be6077f60e7f424a99
Gustavo  Barros <gustavoandrade0125@gmail.com>
2023-06-17 09:42:49 -0300

    Problema do delete

--- backend/src/core/controllers/cargo/deleteController.js
+++ backend/src/core/controllers/cargo/deleteController.js
@@ -3,14 +3,14 @@ import { deleteCargoService } from "../../service/cargo/delete";
 
 export async function deleteCargo(req = request, res = response) {
     const cargoId = req.params.id;
-    try {
-        const cargo = await deleteCargoService(cargoId);
-        if (!cargo) {
+    try { 
+        const deletar = await deleteCargoService(cargoId);
+        if (!deletar) {
             return res.status(404).json({ error: 'Equipe não encontrada' });
         }
-    } catch(error){
-        console.error('Erro ao deletar cargo:', error);
-        res.status(500).json({ error: 'Erro ao deletar cargo' });
-    }
+        res.json(deletar);
+    } catch (error) {
+        console.error('Erro ao buscar cargos:', error);
+        res.status(500).json({ error: 'Erro ao buscar cargos' });
+      }
 }
-
