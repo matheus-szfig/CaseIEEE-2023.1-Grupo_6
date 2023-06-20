9547ba3fd07cdad0686b0080b6b11fdeb60472be
Matheus de Souza Figueiredo <matheus.szfig@gmail.com>
2023-06-20 14:25:26 -0300

    perfil-done

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { AuthComponent } from "../components/Auth";
+import EquipesTab from "../components/admin/EquipesTab";
+import MembrosTab from "../components/admin/MembrosTab";
 
 const Home = () => {
   return (
-    <>
+    <AuthComponent permissions={['admin']} redirect={'/'}>
         <Navbar/>
-
-    </>
+        <div className='mx-5 mt-10 sm:mx-[4.5rem]'>
+          <h1 className='text-3xl font-bold text-primary'>Painel</h1>
+          <div className='mt-6 block md:flex'>
+            <EquipesTab />
+            <MembrosTab />
+          </div>
+        </div>
+    </AuthComponent>
   );
 }
 
