8f43e1e2f467721f691a3fc8886b9994db4c574a
Arthur Duarte <duartearthur42@gmail.com>
2023-06-20 22:06:13 -0300

    Approve cargo

--- frontend/src/components/admin/Membro.jsx
+++ frontend/src/components/admin/Membro.jsx
@@ -4,17 +4,26 @@ import EditBtn from "../profile/EditButton";
 import { equipeSelected } from "./EquipesTab";
 import { useEffect } from "react";
 import { removeCargosModalShowAtom, cargoRemovingAtom } from "../profile/ModalRemoveCargos"
+import { approveCargosModalShowAtom, cargoApprovingAtom } from "./ModalApproveCargos"
+import ApproveBtn from "./ApproveButton";
 
 export default function MemberComponent({ id, nome, cargos }) {
 	const [selected, setSelected] = useRecoilState(equipeSelected);
 
-  const [_c, setCargo] = useRecoilState(cargoRemovingAtom);
+  const [_rc, setRemovedCargo] = useRecoilState(cargoRemovingAtom);
+  const [_ac, setApprovedCargo] = useRecoilState(cargoApprovingAtom);
   const [_r, setRemovingCargo] = useRecoilState(removeCargosModalShowAtom);
+  const [_a, setApprovingCargo] = useRecoilState(approveCargosModalShowAtom);
 
   function RemoveCargo (e, id_usuario, id_equipe, id_cargo) {
     e.preventDefault();
     setRemovingCargo(true);
-    setCargo({id_usuario, id_equipe, id_cargo});
+    setRemovedCargo({id_usuario, id_equipe, id_cargo});
+  }
+  function ApproveCargo (e, id_usuario, id_equipe, id_cargo) {
+    e.preventDefault();
+    setApprovingCargo(true);
+    setApprovedCargo({id_usuario, id_equipe, id_cargo});
   }
 	return (
 		<div className={"mb-1 flex flex-col px-4 border rounded bg-primary"}>
@@ -34,8 +43,12 @@ export default function MemberComponent({ id, nome, cargos }) {
               ${!!(i !== 0) ? "border-t" : ""}`}
 							>
 								<h4 className={"text-md font-medium text-white"}>{c.cargo}</h4>
-                {!c.aprovado ? "componente botao" : null}
-								<CancelBtn onClick={(e)=>RemoveCargo(e, id, c.id_equipe, c.id_cargo)} className="bg-white hover:bg-gray-300 rounded w-6  font-bold hover:text-white text-primary" />
+                				<div >
+									{!c.aprovado ?
+										<ApproveBtn onClick={(e)=>ApproveCargo(e, id, c.id_equipe, c.id_cargo)} className="mr-2 bg-white hover:bg-gray-300 rounded w-6  font-bold hover:text-white text-primary"/>
+									: null}
+									<CancelBtn onClick={(e)=>RemoveCargo(e, id, c.id_equipe, c.id_cargo)} className="bg-white hover:bg-gray-300 rounded w-6  font-bold hover:text-white text-primary" />
+								</div>
 							</div>
 						);
 					})}
--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -4,12 +4,14 @@ import { AuthComponent } from "../components/Auth";
 import EquipesTab from "../components/admin/EquipesTab";
 import MembrosTab from "../components/admin/MembrosTab";
 import ModalRemoveCargos from "../components/profile/ModalRemoveCargos";
+import ModalApproveCargos from "../components/admin/ModalApproveCargos";
 
 const Home = () => {
   return (
     
     <AuthComponent permissions={['admin']} redirect={'/'}>
         <ModalRemoveCargos text="Deseja realmente apagar o cargo deste usuário?"/>
+        <ModalApproveCargos text={"Deseja aprovar o cargo deste usuário?"}/>
         <Navbar/>
         <div className='mx-5 mt-10 sm:mx-[4.5rem]'>
           <h1 className='text-3xl font-bold text-primary'>Painel</h1>
