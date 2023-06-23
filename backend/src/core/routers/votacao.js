bc64def29804108d660dffac2743a872cd2148e9
Arthur Duarte <duartearthur42@gmail.com>
2023-06-23 13:12:22 -0300

    Update criado e create alterado

--- backend/src/core/routers/votacao.js
+++ backend/src/core/routers/votacao.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import findAll from "../controllers/votacao/getVotacao";
 import create from "../controllers/votacao/createVotacao";
+import update from "../controllers/votacao/updateVotacao";
 
 
 const router_votacao = Router();
 
 router_votacao.get("/", findAll);
 router_votacao.post("/create", create);
+router_votacao.patch("/update", update);
 
 export default router_votacao;
