2265c8369afaa11381516a8f13d54332eb870ba1
Matheus de Souza Figueiredo <matheus.szfig@gmail.com>
2023-06-23 23:10:05 -0300

    open and close votação done

--- backend/src/core/controllers/votacao/createVotacao.js
+++ backend/src/core/controllers/votacao/createVotacao.js
@@ -2,8 +2,8 @@ import { request, response } from "express";
 import createVotacao from "../../service/votacao/createVotacao";
 
 export default async function create(req, res) {
-  const { titulo } = req.body;
   const data_inicio = new Date();
+  const titulo = 'Votação '+ data_inicio.toISOString().split('T')[0].split('-').splice(0, 2).reverse().toString().replace(/\,/g, '/');
   const cadastrate = await createVotacao(titulo, data_inicio);
   return res.json(cadastrate);
 }
--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { AuthComponent } from "../components/Auth";
 import EquipesTab from "../components/admin/EquipesTab";
@@ -9,8 +9,52 @@ import ModalUpdateEquipes from "../components/admin/ModalUpdateEquipe";
 import ModalDeleteEquipes from "../components/admin/ModalDeleteEquipe";
 import ModalCreateEquipes from "../components/admin/ModalCreateEquipe";
 import ModalDeleteUser from "../components/admin/ModalDeleteUser";
+import UseApi from "../hooks/useApi";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
-const Home = () => {
+const Admin = () => {
+
+  const [votAberta, setVotacaoAberta] = useState(null);
+  const api = useCallback(UseApi, [])()
+  const navigate = useNavigate();
+
+  async function LoadRes () {
+    const votacoes = await api.get("/votacao");
+    const votAb = votacoes?.data?.find(v => !v.data_fim);
+    setVotacaoAberta(votAb?.id ||  null);
+  }
+
+  async function AbrirVotacao (e) {
+    e.preventDefault();
+    try{
+      const result = api.post("/votacao/create");
+      toast.promise(result, {
+        success:"Votação aberta com sucesso!",
+        error:"Não foi possível completar sua solicitação!"
+      })
+      await result;
+      setTimeout(() => navigate(0), 1000);
+    }catch(e){}
+  }
+
+  async function FecharVotacao (e) {
+    e.preventDefault();
+    try{
+      const result = api.patch("/votacao/update", {id:votAberta});
+      toast.promise(result, {
+        success:"Votação fechada com sucesso!",
+        error:"Não foi possível completar sua solicitação!"
+      })
+      await result;
+      setTimeout(() => navigate(0), 1000);
+    }catch(e){}
+  }
+
+  useEffect(() => {
+    LoadRes();
+  }, [])
+
   return (
     <AuthComponent permissions={["admin"]} redirect={"/"}>
       <ModalRemoveCargos text="Deseja realmente apagar o cargo deste usuário?" />
@@ -25,9 +69,19 @@ const Home = () => {
           <h1 className="text-3xl font-bold text-primary">
             Painel de Administrador
           </h1>
-          <button className=" bg-gray-100 hover:bg-primary hover:text-white text-primary font-bold py-2 px-4 outline outline-3 -outline-offset-4 outline-primary rounded mb-10">
-            Iniciar Votação
-          </button>
+
+          {
+            !votAberta ?
+            <button className=" bg-gray-100 hover:bg-primary hover:text-white text-primary font-bold py-2 px-4 outline outline-3 -outline-offset-4 outline-primary rounded mb-10"
+            onClick={AbrirVotacao}>
+              Abrir Votação
+            </button> :
+            <button className=" bg-gray-100 hover:bg-red-600 hover:text-white text-red-600 font-bold py-2 px-4 outline outline-3 -outline-offset-4 outline-red-600 rounded mb-10"
+            onClick={FecharVotacao}>
+              Fechar Votação
+            </button>
+          }
+          
         </div>
         <div className="mt-6 block md:flex">
           <EquipesTab />
@@ -38,4 +92,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Admin;
